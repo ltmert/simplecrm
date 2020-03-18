e168399e0e5077580582a5e1c3b93ef6930c88f0
ltmert <mert.gokkaya@gmail.com>
2020-03-18 00:09:27 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Simple CRM from CSIC to N2N
+    </title>
+</head>
+<body>
+<h1>Simple, custom and effective CRM</h1>
+
+<div>
+<p>
+Customer Relationship Management (CRM) is a system that tracks, records and assists customer interactions and data throughout the customer lifecycle between the customer and the company across different channels. 
+</p>
+
+<p>
+In the simple CRM system, company can have data to make decisions and improve sales and marketing efforts with built in AI:
+</p>
+
+<ul>
+<li>Access their tasks</li>
+<li>View their leads </li>
+<li>Create new tasks for each lead</li>
+<li>Create new opportunity</li>
+<li>Lose a sale</li>
+</ul>
+
+<p>
+Sales managers will be able to:
+</p>
+
+<ul>
+<li>Manage all customers </li>
+<li>Manage sales team</li>
+<li>View current sales activities</li>
+</ul>
+</div>
+
+<div>
+    <h2>start CRM:</h2>
+    <div>   
+        <a href="sales/tasks.php">Sales</a> | <a href="managers/pipeline.php">Manager</a>
+    </div>
+</div>
+
+</body>
+</html>
