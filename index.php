80f7fb2f240ae3699e6a82522c953ba7d476ae50
Mert <mert.gokkaya@gmail.com>
2020-03-25 19:53:30 -0400

    initiated bootstrap nd login form

--- index.php
+++ index.php
@@ -5,11 +5,62 @@
         Simple CRM from CSIC to N2N
     </title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:800" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="/simplecrm/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--<link rel="stylesheet" type="text/css" href="/simplecrm/css/style.css">-->
 </head>
 <body>
 <!--<h1>Simple, custom and effective CRM</h1>-->
-<div class="page">
+   <?php
+    $email= $_POST['email'];
+    $name= $_POST['user'];
+    $password = $_POST['password'];    
+    if(isset($_POST['submit'])) {
+      // Check if name has been entered
+      if(empty($_POST['user'])) {
+        $errName= 'Please enter your user name';
+      }
+      // Check if email has been entered and is valid
+      else if(empty($_POST['email'])) {
+        $errEmail = 'Please enter a valid email address';
+      }
+      // check if a password has been entered and if it is a valid password
+      else if(empty($_POST['password']) || (preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $_POST["password"]) === 0)) {
+        $errPass = '<p class="errText">Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit</p>';
+      } else {
+        echo "The form has been submitted";
+      }
+    }
+  ?>
+  <div class="page">
+<form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <div class="form-group row">
+        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+          <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputUser" class="col-sm-2 col-form-label">User Name</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputUser" name="user" placeholder="Username">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="offset-sm-2 col-sm-10">
+          <input type="submit" value="Sign in" name="submit" class="btn btn-primary"/>
+        </div>
+      </div>
+    </form>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   
 <div class="row_1">
 <div class="column_1">
     <header>
